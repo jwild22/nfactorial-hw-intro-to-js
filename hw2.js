693f5b994d74632a079640ea9cf64a1eff4430c4
Farukh K <farukhk@Farukhs-MacBook-Air.local>
2024-01-12 11:55:14 +0600

    submission

--- hw2.js
+++ hw2.js
@@ -1,29 +1,29 @@
 let restaraunt = 'Liitle Brazil';
 const PLAYER_NUMBER = 22;
 let shop = {
-    name: 'Magnum',
-    isOpen: true,
-}
+  name: 'Magnum',
+  isOpen: true,
+};
 
 shop['quantity'] = 50;
 shop['competitors'] = undefined;
 
-let vehicle= {};
-vehicle.brandName = 'BMW'
+let vehicle = {};
+vehicle.brandName = 'BMW';
 vehicle.model = 'X5';
 vehicle.model = 'M1';
 delete vehicle.model;
 
 let salaries = {
-	Aroo: 100,
-	Dalida: 160,
-	Samat: 130
-}
+  Aroo: 100,
+  Dalida: 160,
+  Samat: 130,
+};
 
 let total = 0;
 
 for (let name in salaries) {
-    total += salaries[name];
+  total += salaries[name];
 }
 
-console.log(total)
+console.log(total);
